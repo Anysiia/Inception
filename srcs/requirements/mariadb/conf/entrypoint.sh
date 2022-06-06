927891e439b28cc0acaff787deb362706c38c724
Celine MOREL A L'HUISSIER <cmorel-a@e3r8p1.clusters.42paris.fr>
2022-06-06 16:24:45 +0200

    Mariadb test to do better

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,35 +1,48 @@
 #!/bin/sh
 
-#Install batabase
-/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+if [ ! d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
 
-#Launch mysqld to operate changes
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+if [ ! d "/var/lib/mysql/mysql" ]; then
+    chown -R mysql:mysql /var/lib/mysql
 
-#################CONFIG####################################################
-#Delete anonymous users
-mysql -e "DELETE FROM mysql.user WHERE User='';"
+    #Init db
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-#Remove test database
-mysql -e "DROP DATABASE IF EXISTS test;"
-mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+    #Create tmp file
 
-#Create database 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+    tmp=`mariadb_tmp`
+    if [ ! -f "$tmp" ]; then
+        return 1
+    fi
 
-#Add user
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+    cat << EOF > $tmp
+USE mysql;
+FLUSH PRIVILEGES;
+    
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    
+CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER IF NOT EXISTS ${MARIADB_USER}'@'%' IDENTIFIED BY ${MARIADB_PASSWORD};
+GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
 
-#Add rights for user on MARIADB_DATABASE
-mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+EOF
+    #Run 
+    /usr/bin/mysqld --user=mysql --bootstrap < $tmp
+    rm -f $tmp
+fi
 
-#Change root password
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-
-#Apply change
-mysql -e "FLUSH PRIVILEGES"
-###########################################################################
+#Config to allow connections
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 #Stop and relaunch mysql
-pkill mysqld
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+
+exec /usr/bin/mysqld --user=mysql --console
