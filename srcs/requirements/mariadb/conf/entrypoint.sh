d1ba59873494c3d2bcab9fe43d37d49034f4cd79
Celine MOREL A L'HUISSIER <cmorel-a@e3r6p1.clusters.42paris.fr>
2022-06-07 10:06:52 +0200

    add debug echo

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -17,23 +17,29 @@ then
 
     /usr/bin/mysqld --user=mysql
 
+    echo "un"
     sudo mysql -e "USE mysql;"
     sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
     sudo mysql -e "DROP DATABASE IF EXISTS test;"
     sudo mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
     sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" 
     
+    echo "two"
     sudo mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;"
     sudo mysql -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}'@'%' IDENTIFIED BY ${MARIADB_PASSWORD};"
     sudo mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}'@'%';"
     sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
     sudo mysql -e "FLUSH PRIVILEGES;"
+    
+    echo "three"
     pkill mysqld
 
 fi
 
+echo "edit .cnf file"
 #Config to allow connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
+echo "relaunch"
 exec /usr/bin/mysqld --user=mysql --console
