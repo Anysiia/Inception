24836425d103ee30031922b5732458ceda5b25a5
Celine MOREL A L'HUISSIER <cmorel-a@e3r6p1.clusters.42paris.fr>
2022-06-07 10:45:54 +0200

    Nooooooo

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#Config to allow connections
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
 if [ ! -d "/run/mysqld" ]
 then
     mkdir -p /run/mysqld
@@ -21,14 +17,12 @@ then
 
     /usr/bin/mysqld --user=mysql & sleep 2
 
-    echo "un"
     mysql -e "USE mysql;"
     mysql -e "DELETE FROM mysql.user WHERE User='';"
     mysql -e "DROP DATABASE IF EXISTS test;"
     mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
     mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" 
     
-    echo "two"
     mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;"
     mysql -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}'@'%' IDENTIFIED BY ${MARIADB_PASSWORD};"
     mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}'@'%';"
@@ -37,5 +31,9 @@ then
 
     pkill mysqld
 fi
+
+#Config to allow connections
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 /usr/bin/mysqld --user=mysql
