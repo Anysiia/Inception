1b9832cbd3de6a521b397bb5ce1e0ee41dc62bd9
Celine MOREL A L'HUISSIER <cmorel-a@e3r6p1.clusters.42paris.fr>
2022-06-07 10:00:40 +0200

    it's weird

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d "/run/mysqld"]
+if [ ! -d "/run/mysqld" ]
 then
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld
@@ -17,17 +17,17 @@ then
 
     /usr/bin/mysqld --user=mysql
 
-    mysql -e "USE mysql;"
-    mysql -e "DELETE FROM mysql.user WHERE User='';"
-    mysql -e "DROP DATABASE IF EXISTS test;"
-    mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
-    mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" 
+    sudo mysql -e "USE mysql;"
+    sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
+    sudo mysql -e "DROP DATABASE IF EXISTS test;"
+    sudo mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+    sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" 
     
-    mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;"
-    mysql -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}'@'%' IDENTIFIED BY ${MARIADB_PASSWORD};"
-    mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}'@'%';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
+    sudo mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    sudo mysql -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}'@'%' IDENTIFIED BY ${MARIADB_PASSWORD};"
+    sudo mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}'@'%';"
+    sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+    sudo mysql -e "FLUSH PRIVILEGES;"
     pkill mysqld
 
 fi
