3a23644a68874ce784b43313bf0f698dcd1e3bc0
Anysia74@hotmail.com <Anysia>
2022-06-13 11:38:30 +0200

    Wp is now visible but need to add php packages

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define ('DB_NAME',          getenv('MARIADB_DATABASE'));
-define ('DB_USER',          getenv('MARIADB_USER'));
-define ('DB_PASSWORD',      getenv('MARIADB_PASSWORD'));
+define ('DB_NAME',          'inceptiondb');
+define ('DB_USER',          'cmorel-a');
+define ('DB_PASSWORD',      'inception42');
 define ('DB_HOST',          'mariadb:3306');
 
 define ('DB_CHARSET',       'utf8');
@@ -16,7 +16,7 @@ define('SECURE_AUTH_SALT',  'uDXK[;@[^9C-e[R-bC.sy*x-]!|m8tZ|dKr|2]<[fG.+k4)A8Aa
 define('LOGGED_IN_SALT',    'K,gl$ad0}bHMfSiPzZsZ`Xx/7Jx+XdU62InW?*N0Ez5]&n||aA5{1aw1ub:uP*?-');
 define('NONCE_SALT',        't~z`zDt8u.+>h+Zg8CNr797O y4?p;ul$Mw%k{O3)as`4a/_Gt`{x7HBK}xin#WI');
 
-$table_prefix = getenv('WP_PREFIX');
+$table_prefix = 'inception42_';
 
 define ('WP_DEBUG', true);
 if ( ! defined('ABSPATH')) {
